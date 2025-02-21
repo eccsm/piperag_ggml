ef70a3155f47d1bd12c7574aa965ecb552d9be83
eccsm <eccsm34@gmail.com>
2025-02-22 00:40:56 +0300

    adj3

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
 class Config:
-    JSON_PATH = os.path.join("train", "data", "Ekincan_Casim_Chunks.json")
-    MODEL_PATH = r"C:\models\qtz8-vicuna-7b-v1.5.gguf"
-    CHROMA_DIR = "chroma_db"
-    RAG_KEYWORDS = ["ekincan", "casim","sug","pep","ecem"]
+    JSON_PATH = os.path.join("train", "data", "yourragfile.json")
+    MODEL_PATH = r"C:\models\qtz8-vicuna-7b-v1.5.gguf" # quantized model with llama.cpp
+    CHROMA_DIR = "chroma_db" # or FAISS
+    RAG_KEYWORDS = ["rag_trigger"]
     FREE_PROMPT_TEMPLATE = (
-        "You are a funny cat named {cat_name}. Engage in a casual conversation.\n"
+        "YOUR PROMPT\n"
         "User: {query}\nCat:"
     )
